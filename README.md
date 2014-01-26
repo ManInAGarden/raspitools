7366826490fdad7190e92b30c1b223e92fec42d8
ManInAGarden <hspeich@versanet.de>
2014-01-26 10:53:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Raspitools
+==========
+
+A collecion of my tools which I use with my various Raspberry Pi boards to make them work like I want. Because 
+python (at least for me) is the standard programming language on the Raspberry Pi all these tools are written in python. Python also
+has the advantage that it is already there when you set your Raspi with a raspian operating system.
+
+Logoutdialog
+------------
+
+Logoutdialog is a small python application to logout from the system or init a shutdown or reboot. When I changed 
+my Raspberry PI to not use lxde any more and instead have openbox only I missed the logout dialog. I did not find a way 
+to use the standard logout from lxde and so I wrote my own in python.
---
+++ logoutdialog.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+#
+# ------------------------------------
+# logoutdialog
+# ------------------------------------
+# 12.01.2014 - HSP - angelegt
+# ------------------------------------
+
+from tkinter import *
+from tkinter import messagebox
+
+import os
+
+
+class LogoutDialog:
+    """Dialog class for the shutdown/reboot/logout/... application"""
+    def __init__(self, title):
+        self.create_gui(title)
+        self.loaded()
+
+    def loaded(self):
+        pass
+
+    def are_you_sure(self, text="Bist du sicher?"):
+        return messagebox.askquestion("Rückfrage", text)
+        
+    def create_gui(self, title):
+        self.root = Tk()
+        self.root.title(title)
+        frame = Frame(self.root, width=400, height = 300, padx=5, pady=5)
+        frame.pack()
+        r=0
+        c=0
+        self.make_button(frame, text="Ruhemodus",
+                         row=r, column=c,
+                         command=self.hibernate)
+        r += 1
+        self.make_button(frame, text="Konsole",
+                         row=r, column=c,
+                         command=self.exit_openbox)
+        r += 1
+        self.make_button(frame, text="Neustart",
+                         row=r,column=c,
+                         command=self.reboot)
+        r += 1
+        self.make_button(frame, text="Ausschalten",
+                         row=r,column=c,
+                         command=self.shutdown)
+        r += 1
+        self.make_button(frame, text="Abbrechen",
+                         row=r,column=c,
+                         command=self.cancel)
+        r += 1
+
+    def mainloop(self):
+        """enters the main loop of Tk"""
+        self.root.mainloop()
+
+    def close(self):
+        """close this application"""
+        self.root.destroy()
+
+    def make_button(self, parent, text="Button", command=None, row=0, column=0):
+        """create a new button widget"""
+        but = Button(parent, text=text, command=command, relief='flat', padx=5, pady=5)
+        but.grid(row=row, column=column, sticky=EW)
+        return but
+        
+    def cancel(self):
+        """callback for cancel-button"""
+        self.close()
+
+    def reboot(self):
+        """callback for reboot-button"""
+        if self.are_you_sure("Bist du sicher, dass du den Rechner neu starten willst?")=='yes':
+            ret = os.system("sudo shutdown -r now")
+
+    def exit_openbox(self):
+        """callback for openbox exit, back to conole"""
+        if self.are_you_sure("Bist du sicher, dass du zur Konsole zurück willst?")=='yes':
+            ret = os.system("openbox --exit")
+
+    def shutdown(self):
+        """callback for shutdown-button"""
+        if self.are_you_sure("Bist du sicher, dass du den Rechner herunterfahren willst?")=='yes':
+            ret = os.system("sudo shutdown -h now")
+        
+    def hibernate(self):
+        """callback for hybernate button"""
+        if self.are_you_sure()=='yes':
+            ret = os.system("gdm-control --suspend")
+
+if __name__ =="__main__":
+    cls = LogoutDialog("Logoff")
+    cls.mainloop()
